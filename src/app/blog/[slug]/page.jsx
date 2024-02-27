f640da29bae827b19142ee96eea1939b9db2be80
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2024-02-27 11:46:47 +0700

    create single post page

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,6 +1,30 @@
+import Image from 'next/image'
+import styles from './singlePostPage.module.css'
+
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="https://images.pexels.com/photos/19682307/pexels-photo-19682307/free-photo-of-hoa-mua-he-cay-b-i-v-n.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image" fill className={styles.img} />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>title</h1>
+        <div className={styles.detail}>
+          <Image src="https://images.pexels.com/photos/19682307/pexels-photo-19682307/free-photo-of-hoa-mua-he-cay-b-i-v-n.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="image" className={styles.avatar} width={50} height={50} />
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Author</span>
+            <span className={styles.detailValue}>Trinh Long Vu</span>
+          </div>
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>01.01.2024</span>
+          </div>
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, corporis? Amet consequatur at quae. Possimus voluptatem maxime neque quasi nam magnam dolore? Impedit accusantium explicabo fuga earum reprehenderit iure itaque.
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/app/components/navbar/links/Link.jsx
+++ src/app/components/navbar/links/Link.jsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import styles from './links.module.css';
 import NavLink from './navLink/navLink';
+import Image from 'next/image';
 
 
 const session = true;
@@ -38,7 +39,7 @@ const Links = () => {
           )
         }
       </div>
-      <button className={styles.menuButton} onClick={() => setOpen((prev) => !prev)}>Menu</button>
+      <Image src="/menu.png" alt='' width={30} height={30} onClick={() => setOpen((prev) => !prev)} className={styles.menuButton} />
       {open && (
         <div className={styles.mobileLinks}>
           {links.map((link) => (
