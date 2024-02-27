612a36654781e293603d5e93ba810596c80128c2
Trịnh Long Vũ <81180330+trinhvu1711@users.noreply.github.com>
2024-02-27 09:42:41 +0700

    create contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,22 @@
+import Image from 'next/image'
+import styles from './contact.module.css'
+
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/contact.png" alt="Contact" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder='Name and Surname' />
+          <input type="text" placeholder='Email address' />
+          <input type="text" placeholder='Phone number (Optional)' />
+          <textarea placeholder='Your message' cols={30} rows={10}></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
